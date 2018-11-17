1ef8ab404065da24ea4bc09d83c4701d0e7114c1
armikhalev <armikhalev@gmail.com>
2018-11-17 00:23:00 -0500

    Clean-up git

--- skarbnik-linux-x64/resources/app/js/out_front/skarbnik_front/conf.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.473 {:static-fns true, :optimize-constants true, :elide-asserts true}
-goog.provide('skarbnik_front.conf');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-skarbnik_front.conf.setting = new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$my_DASH_env,"this-is-prod"], null);
