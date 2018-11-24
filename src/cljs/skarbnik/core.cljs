5d5e7ae346421781b757cab5f924cc49abe9ef97
armikhalev <armikhalev@gmail.com>
2018-11-24 01:13:17 -0500

    Adds datepicker

--- src/cljs/skarbnik/core.cljs
+++ src/cljs/skarbnik/core.cljs
@@ -5,7 +5,9 @@
 
 (nodejs/enable-util-print!)
 
-(defonce state (atom {:data ""}))
+(defonce state (atom {:data      []
+                      :from-date ""
+                      :to-date   ""}))
 
 ;; nodejs
 
@@ -41,8 +43,48 @@
               (fn [err content]
                 (if err
                   (prn err)
-                  (swap! state update-in [:data] str content)))))
+                  (swap! state assoc :data (utils/scv->maps content))))))
+
 ;; ENDs file management fns
+
+
+;; Dates
+(defn mdy->ymd
+  "Converts date format MM/DD/YYYY to YYYY-MM-DD."
+  [mdy]
+  (let [date (clojure.string/split mdy #"/"),
+        year (last date),
+        rest- (take 2 date)]
+    (clojure.string/join "-" (conj rest- year ))))
+
+
+(defn date->ints
+  "Converts date string of form YYYY-MM-DD to list of integers."
+  [date]
+  (vec (map js/parseInt (clojure.string/split date #"-"))))
+
+
+(defn compare-dates
+  "Compares two lists of dates integers of form YYYY-MM-DD using comparison operator.
+   Returns boolean."
+  [comparator-symbol first-date second-date]
+  (not (some false?
+            (map
+             (partial comparator-symbol)
+             first-date second-date))))
+
+
+(defn filter-by-date
+  "Filters out entries that are bigger than `from-date` and less than `to-date`.
+   Expects `entry` to have key `Trans.Date`."
+  [entries from-date to-date]
+  (filter
+   (fn [entry]
+     (let [date (:Trans.Date entry)]
+       (and (compare-dates >= (date->ints (mdy->ymd date)) (date->ints from-date))
+            (compare-dates <= (date->ints (mdy->ymd date)) (date->ints to-date)))))
+   entries))
+;;
 
 
 ;; Root
@@ -83,11 +125,31 @@
            [:td (:Category entry)]]))
 
       ;; feed `map-indexed`
-      (utils/scv->maps (:data @state)))]]
+      (:data @state))]]
 
-   (let [debt (utils/get-total (utils/scv->maps (:data @state)) >)
-         paid (utils/get-total (utils/scv->maps (:data @state)) <)]
-     [:section
+   [:section.date-picker
+    [:label "Select date range from: "]
+    [:input
+     {:type "date"
+      :on-change #(swap! state assoc :from-date (.-target.value %))
+      :name "from-date"}]
+    [:label " to: "]
+    [:input
+     {:type "date"
+      :on-change #(swap! state assoc :to-date (.-target.value %))
+      :name "to-date"}]]
+
+   [:button
+    {:on-click #(swap! state assoc :data (filter-by-date
+                                          (:data @state)
+                                          (:from-date @state)
+                                          (:to-date @state)))}
+    "Filter by date"]
+
+   (let [debt (utils/get-total (:data @state) >)
+         paid (utils/get-total (:data @state) <)]
+
+     [:section.sums
 
       ;; sum `debt` and `paid`, since paid is always negative
       [:h2 "Still owe: " (utils/cents->dollars
@@ -95,7 +157,6 @@
                              (utils/dollars->cents paid)))]
 
       [:h2 "Total historical debt: " debt]
-
       [:h2 "Total historical paid: " paid]])
    ])
 
