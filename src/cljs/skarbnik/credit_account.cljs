e64078559831f22b3d82ba0925eca0f65c7a597e
armikhalev <armikhalev@gmail.com>
2019-03-18 23:18:36 -0400

    WIP: figure out full join for bigs-and-debts and data

--- src/cljs/skarbnik/credit_account.cljs
+++ src/cljs/skarbnik/credit_account.cljs
@@ -62,13 +62,32 @@
      [:h3 (str "Initial Balance: " (logic/cents->dollars (:initial-credit-balance @state)))]
 
      [:hr]
-     (components/transactions-table
-      {:state                   state
-       :data                    data
-       :credit?                 true
-       :account-data-$key       :credit-data
-       :account-recur-data-$key :credit-recur-data
-       :account-big-data-$key   :credit-big-data})
+
+     ;; Add `:bigs` and `:debt`
+     ;; TODO: add logic to merge data with bigs-and-paids
+     (let [paids*                  (logic/payments data)
+           paids                   (logic/str-dates->cljs-time paids*)
+           bigs*                   (-> @state :credit-big-data vals)
+           bigs                    (logic/str-dates->cljs-time bigs*)
+           paids-with-bigs         (logic/paids-with-bigs paids bigs)
+           data-with-bigs-and-debt (logic/reduce-bigs-and-paids paids-with-bigs)
+           back-to-str-dates       (map
+                                    (fn [m]
+                                      (update
+                                       m :date #(logic/cljs-time->str %)))
+                                    data-with-bigs-and-debt)
+           merged-data             (for [d data
+                                         b back-to-str-dates
+                                         :when  (= (:amount d) (:amount b))]
+                                     (merge d b))]
+       (cljs.pprint/pprint merged-data)
+       (components/transactions-table
+        {:state                   state
+         :data                    data
+         :credit?                 true
+         :account-data-$key       :credit-data
+         :account-recur-data-$key :credit-recur-data
+         :account-big-data-$key   :credit-big-data}))
 
      [:hr]
      (components/date-picker state data :credit-data)
