a76a5d2c35b9f5a1f239eb25d72ec5eccb3926d2
armikhalev <armikhalev@gmail.com>
2019-05-01 07:45:20 -0400

    Tests:Adds merge-bigs-debt-and-data-test

--- test/skarbnik/core_test.cljs
+++ test/skarbnik/core_test.cljs
@@ -3,6 +3,7 @@
             [reagent.core :as r]
             [devcards.core :refer-macros [deftest defcard-rg]]
             [skarbnik.logic :as logic]
+            [cljs-time.coerce :as ->cl-time]
             [skarbnik.db :as sdb]
             [skarbnik.components :as components]))
 
@@ -35,18 +36,29 @@
 
 (bank-recur-data! {"Loan" {:description "QUICKEN LOANS MTG PYMTS 120518",
                            :amount -2000,
-                           :date " 12/06/2018"},
+                           :date "12/06/2018"},
                    "Discover" {:description "DISCOVER E-PAYMENT 181008",
                                :amount -3000,
-                               :date " 10/10/2018"}})
+                               :date "10/10/2018"}})
 
 (credit-recur-data! {"Bank" {:description "DISCOVER E-PAYMENT 181008",
-           :amount 3000,
-           :date " 10/10/2018"}})
+                             :amount 3000,
+                             :date "10/10/2018"}})
 
 
 (def test-data
-  '({:date "11/26/2018", :description " MBTA PAY BY PHO BOSTON /MA US CARD PURCHASE", :BAICode " ", :amount -2500, :SerialNum " 2018112600000001"} {:date "11/13/2018", :description " ZINAIDA LEVIN M CANTON /MA US CARD PURCHASE", :BAICode " ", :amount -2500, :SerialNum " 2018111300000001"} {:date "11/13/2018", :description " CASH WITHDRAWAL SANTANDER D199 Holbrook /MA US", :BAICode " ", :amount -5000, :SerialNum " 2018111300000002"} {:date "11/13/2018", :description " INTERNET TRANSFER FROM ACCT *2394 - SANTANDER SAVINGS", :BAICode " ", :amount 5000, :SerialNum " 2018111300000003"}))
+  '({:date "11/26/2018",
+     :description "MBTA PAY BY PHO BOSTON /MA US CARD PURCHASE",
+     :amount -2500}
+    {:date "11/13/2018",
+     :description "ZINAIDA LEVIN M CANTON /MA US CARD PURCHASE",
+     :amount -2500}
+    {:date "11/13/2018",
+     :description "CASH WITHDRAWAL SANTANDER D199 Holbrook /MA US",
+     :amount -5000}
+    {:date "11/13/2018",
+     :description "INTERNET TRANSFER FROM ACCT *2394 - SANTANDER SAVINGS",
+     :amount 5000}))
 
 ;;; ENDs: prepare data
 
@@ -127,3 +139,106 @@
                                :credit-total-difference! credit-total-difference!}])
 
 ;; ENDs: Credit
+
+;; LOGIC
+
+(defn dates->cljs-time
+  [date]
+  (->cl-time/from-date (js/Date. date)))
+
+(def test-credit-data
+  '({:date "12/02/2017",
+     :description "  Lots of Bad Services",
+     :amount 10000,
+     :Category "  Services"}
+    {:date "12/05/2017",
+     :description "  spasibo vashu za nogu",
+     :amount -1000,
+     :Category "  Payments and Credits"}
+    {:date "12/01/2018",
+     :description "  JMF LANDSCAPING HOLBROOK MA",
+     :amount 10000,
+     :Category "  Services"}
+    {:date "12/03/2018",
+     :description "  MBTA MTICKET BOSTON MA",
+     :amount 20000,
+     :Category "  Travel/ Entertainment"}
+    {:date "12/05/2018",
+     :description "  INTERNET PAYMENT - THANK YOU",
+     :amount -10000,
+     :Category "  Payments and Credits"}))
+
+(def back-to-str-dates
+  {:--spasibo-vashu-za-nogu--1000-12-05-2017
+   {:date "12/05/2017",
+    :description "  spasibo vashu za nogu",
+    :amount -1000,
+    :Category "  Payments and Credits",
+    :bigs '({:description "  Lots of Bad Services",
+            :amount 10000,
+            :date (dates->cljs-time  "12/02/2017")}),
+    :debt 9000},
+   :--INTERNET-PAYMENT---THANK-YOU--10000-12-05-2018
+   {:date "12/05/2018",
+    :description "  INTERNET PAYMENT - THANK YOU",
+    :amount -10000,
+    :Category "  Payments and Credits",
+    :bigs '({:description "  Lots of Bad Services",
+            :amount 10000,
+            :date (dates->cljs-time  "12/02/2017")}
+           {:description "  MBTA MTICKET BOSTON MA",
+            :amount 20000,
+            :date (dates->cljs-time "12/03/2018")}),
+    :debt 19000}})
+
+(def expected-bigs-debt-and-data
+  '({:date "12/02/2017",
+     :description "  Lots of Bad Services",
+     :amount 10000,
+     :Category "  Services",
+     :bigs [],
+     :debt 0}
+    {:date "12/05/2017",
+     :description "  spasibo vashu za nogu",
+     :amount -1000,
+     :Category "  Payments and Credits",
+     :bigs
+     ({:description "  Lots of Bad Services",
+       :amount 10000,
+       :date (dates->cljs-time "12/02/2017")}),
+     :debt 9000}
+    {:date "12/01/2018",
+     :description "  JMF LANDSCAPING HOLBROOK MA",
+     :amount 10000,
+     :Category "  Services",
+     :bigs [],
+     :debt 0}
+    {:date "12/03/2018",
+     :description "  MBTA MTICKET BOSTON MA",
+     :amount 20000,
+     :Category "  Travel/ Entertainment",
+     :bigs [],
+     :debt 0}
+    {:date "12/05/2018",
+     :description "  INTERNET PAYMENT - THANK YOU",
+     :amount -10000,
+     :Category "  Payments and Credits",
+     :bigs
+     ({:description "  Lots of Bad Services",
+       :amount 10000,
+       :date (dates->cljs-time "12/02/2017")}
+      {:description "  MBTA MTICKET BOSTON MA",
+       :amount 20000,
+       :date (dates->cljs-time "12/03/2018")}),
+     :debt 19000}))
+
+(deftest merge-bigs-debt-and-data-test
+  (testing
+      "Should return transactions with debts
+       that go over to the next payment (negative amount),
+       with big transactions linked to debt."
+    (is (= (logic/merge-bigs-debt-and-data
+            test-credit-data
+            back-to-str-dates)
+           expected-bigs-debt-and-data))))
+;; ENDS: LOGIC
