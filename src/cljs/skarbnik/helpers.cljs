deeef069082de7f6cf5534a9b39c1f86b98ad970
armikhalev <armikhalev@gmail.com>
2019-06-05 07:44:27 -0400

    Removes `three-fold-key` fn

--- src/cljs/skarbnik/helpers.cljs
+++ src/cljs/skarbnik/helpers.cljs
@@ -6,21 +6,6 @@
   {:class (str "bold margin-left-5 " (if (< number 0) "color-red" "color-blue"))})
 
 
-(defn three-fold-key
-  ;; TODO: Spec it!
-  [entry]
-  (let [desc?  (:description entry)
-        desc*  (string/trim (if desc? desc? " "))
-        desc   (if (string? desc*) (-> desc* (string/replace #" " "")), desc*)
-        amount (:amount entry)
-        date*  (:date entry)
-        date   (if (string? date*) (string/replace date* #"/" "-"), date*)]
-    ;; null-check
-    (when (not-any? nil? [desc amount date])
-      (keyword
-       (string/trim (str  desc "-"  amount "-"  date))))))
-
-
 (defn set-distinct-data!
   "Adds key to `bank-data` as concatenation of `description`, `amount` and `date`
    And value being a map of those key/value pairs in `entry`"
